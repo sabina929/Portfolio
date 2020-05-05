bd29817319b9705e192e2a59dbd55b78b910db6c
Sabina <sabina.abbasova2909@gmail.com>
2020-05-05 19:16:56 +0400

    changed nav styles and animations by using js media match

--- script.js
+++ script.js
@@ -320,13 +320,69 @@ function loading() {
     main.style.display = "block";
     setTimeout(() => (main.style.opacity = 1), 50); 
     
+    // NAVIGATION
+    const mediaQuery1024 = window.matchMedia("(max-width: 1024px)");
+    const mediaQuery801 = window.matchMedia("(max-width: 801px)");
+    const mediaQuery710 = window.matchMedia("(max-width: 710px)");
+    const mediaQuery610 = window.matchMedia("(max-width: 610px)");
 
-    gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
-    gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+    function checkMedia1000ForNav() {
+      if (mediaQuery1024.matches) {
+          gsap.to(".to-left", {x: -500, duration: 5, ease: "linear", yoyo:true, repeat: -1})
+          gsap.to(".to-right", {x: 0, duration: 5, ease: "linear", yoyo:true, repeat: -1})
+          
+      }
+       else {
+          gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+          gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+      }
+    }
+    function checkMedia801ForNav() {
+      if (mediaQuery801.matches) {
+        gsap.to(".to-left", {x: -320, duration: 8, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 8, ease: "linear", yoyo:true, repeat: -1})      
+    }
+     else {
+        gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+    }
+    }
+    function checkMedia710ForNav() {
+      if (mediaQuery710.matches) {
+        gsap.to(".to-left", {x: -240, duration: 9, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 9, ease: "linear", yoyo:true, repeat: -1})      
+    }
+     else {
+        gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+    }
+    }
+    function checkMedia610ForNav() {
+      if (mediaQuery610.matches) {
+        gsap.to(".to-left", {x: -200, duration: 9, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 9, ease: "linear", yoyo:true, repeat: -1})      
+    }
+     else {
+        gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+    }
+    }
+  
+    checkMedia1000ForNav();
+    mediaQuery1024.addListener(checkMedia1000ForNav);
+    checkMedia801ForNav();
+    mediaQuery801.addListener(checkMedia801ForNav);
+    checkMedia710ForNav();
+    mediaQuery710.addListener(checkMedia710ForNav);
+    checkMedia610ForNav();
+    mediaQuery610.addListener(checkMedia610ForNav);
+    
+    // gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
+    // gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
     
     //MOUSE TRACKER
     const mediaQuery = window.matchMedia("(min-width: 1200px)");
-    function myFunction(mediaQuery) {
+    function checkMediaForTracker(mediaQuery) {
       if (mediaQuery.matches) {
         const tracker = document.querySelector("#tracker");
           document.addEventListener("mousemove", e => {
@@ -346,8 +402,8 @@ function loading() {
 
     
     setTimeout(()=>{
-      myFunction(mediaQuery);
-      mediaQuery.addListener(myFunction);
+      checkMediaForTracker(mediaQuery);
+      mediaQuery.addListener(checkMediaForTracker);
       
     }, 2000)
 
@@ -563,6 +619,7 @@ function startGSAPAnimations(){
     contactBack.addEventListener("click", () => tlContact.reverse());
 
 }
+
 document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener(
     "load",
@@ -576,184 +633,11 @@ document.addEventListener("DOMContentLoaded", function() {
   );
 });
 
+
+
+
+
 // GSAP ANIMATIONS
-// gsap.to(".to-left", {x: -720, duration: 15, ease: "linear", yoyo:true, repeat: -1})
-// gsap.to(".to-right", {x: 0, duration: 15, ease: "linear", yoyo:true, repeat: -1})
-
-
-  // let select = (selector) => {
-  //   return document.querySelector(selector);
-  // };
-  
-  // // ABOUT SECTION
-  // let aboutLeft = select("#nav > p.to-left > span:nth-child(1)");
-  // let aboutRight = select("#nav > p.to-right > span:nth-child(4)");
-  // let aboutBack = select("section#about .back");
-
-
-  // let tlAbout = gsap
-  //   .timeline({
-  //     paused: true,
-  //     defaults: { duration: 1.4,transformOrigin: "50% 50%", opacity: 0, ease: "elastic(.1, 1)" }
-  //   })
-
-  //   .fromTo("section#about", {yPercent: 50, xPercent: -50,scale: 0, opacity: 0, borderRadius: "50%"}, {yPercent: -50, xPercent: -50,scale: 1, opacity: 1, borderRadius: "0px"})
-    
-  //   .to(".logo", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-  //   .to(".copyright", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-    
-
-  //   .fromTo("section#about .about-img", {duration: 5, y: -100, opacity: 0, ease: "elastic(2, .1)"}, {
-  //     y:0, opacity:1
-  // }, "<.6")
-
-  //   .from("section#about .splitting .char", {
-  //       y: 40,
-  //       duration: 1.8,
-  //       stagger: 0.008,
-  //       ease: "elastic(.8, 0.0)"
-  //     }, "<.6")
-
-
-  //     .to("#tracker", {
-  //       background:"hsl(56, 19%, 16%)",
-  //       opacity: 1
-  //     }, "<.6")
-
-  //   aboutLeft.addEventListener("click", () => tlAbout.play());
-  //   aboutRight.addEventListener("click", () => tlAbout.play());
-  //   aboutBack.addEventListener("click", () => tlAbout.reverse());
-
-
-  // // ARTORKS SECTION
-  // let artworksLeft = select("#nav > p.to-left > span:nth-child(2)");
-  // let artworksRight = select("#nav > p.to-right > span:nth-child(3)");
-  // let artworksBack = select("section#artworks .back");
-
-
-  // let tlArtworks = gsap
-  //   .timeline({
-  //     paused: true,
-  //     defaults: { duration: 1.4,transformOrigin: "50% 50%", opacity: 0, ease: "elastic(.1, 1)" }
-  //   })
-
-  //   .fromTo("section#artworks", {yPercent: 50,xPercent: -50,scale: 0, opacity: 0, borderRadius: "50%"}, {yPercent: -50, xPercent: -50,scale: 1, opacity: 1, borderRadius: "0px"})
-    
-  
-  //   .to(".logo", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-  //   .to(".copyright", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-    
-
-
-  //   .fromTo("section#artworks .work-img", {duration: 5, y: -100, opacity: 0, ease: "elastic(2, .1)"}, {
-  //       y:0, opacity:1
-  //   }, "<.6")
-
-  //   .from("section#artworks .splitting .char", {
-  //       y: 40,
-  //       duration: 1.8,
-  //       stagger: 0.01,
-  //       ease: "elastic(.8, 0.0)"
-  //     }, "<.6")
-
-
-  //     .to("#tracker", {
-  //       background:"hsl(56, 19%, 16%)",
-  //       opacity: 1
-  //     }, "<.6")
-
-  // artworksLeft.addEventListener("click", () => tlArtworks.play());
-  // artworksRight.addEventListener("click", () => tlArtworks.play());
-  // artworksBack.addEventListener("click", () => tlArtworks.reverse());
-
-  // // WEBSITES SECTION
-  // let websitesLeft = select("#nav > p.to-left > span:nth-child(3)");
-  // let websitesRight = select("#nav > p.to-right > span:nth-child(2)");
-  // let websitesBack = select("section#websites .back");
-
-
-  // let tlWebsites = gsap
-  //   .timeline({
-  //     paused: true,
-  //     defaults: { duration: 1.4,transformOrigin: "50% 50%", opacity: 0, ease: "elastic(.1, 1)" }
-  //   })
-  //   .fromTo("section#websites", {yPercent: 50,xPercent: -50,scale: 0, opacity: 0, borderRadius: "50%"}, {yPercent: -50,xPercent: -50,scale: 1, opacity: 1, borderRadius: "0px"})
-  //   .to(".logo", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-  //   .to(".copyright", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-  //   .fromTo("section#websites .work-img", {duration: 5, y: -100, opacity: 0, ease: "elastic(2, .1)"}, {
-  //       y:0, opacity:1
-  //   }, "<.6")
-  //   .from("section#websites .splitting .char", {
-  //       y: 40,
-  //       duration: 1.8,
-  //       stagger: 0.01,
-  //       ease: "elastic(.8, 0.0)"
-  //   }, "<.6")
-  //   .to("#tracker", {
-  //     background:"hsl(56, 19%, 16%)",
-  //     opacity: 1
-  //   }, "<.6")
-
-  //   websitesLeft.addEventListener("click", () => tlWebsites.play());
-  //   websitesRight.addEventListener("click", () => tlWebsites.play());
-  //   websitesBack.addEventListener("click", () => tlWebsites.reverse());
-
-  //   // CONTACT SECTION
-  // let contactLeft = select("#nav > p.to-left > span:nth-child(4)");
-  // let contactRight = select("#nav > p.to-right > span:nth-child(1)");
-  // let contactBack = select("section#contact .back");
-  
-  // let tlContact = gsap
-  // .timeline({
-  //     paused: true,
-  //     defaults: { duration: 1.4,transformOrigin: "50% 50%", opacity: 0, ease: "elastic(.1, 1)" }
-  //   })
-
-  //   .fromTo("section#contact", {yPercent: 50,xPercent: -50,scale: 0, opacity: 0, borderRadius: "50%"}, {yPercent: -50,xPercent: -50,scale: 1, opacity: 1, borderRadius: "0px"})
-    
-  //   .to(".logo", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<.6")
-  //   .to(".copyright", {
-  //     color:"hsl(251, 60%, 17%)",
-  //     opacity: 1
-  //   }, "<")
-
-    
-  //   .from("section#contact .splitting .char", {
-  //     y: 40,
-  //       duration: 1.8,
-  //       stagger: 0.01,
-  //       ease: "elastic(.8, 0.0)"
-  //     }, "<.6")
-      
-  //   .to("#tracker", {
-  //     background:"hsl(56, 19%, 16%)",
-  //     opacity: 1
-  //   }, "<.6")
-      
-  //   contactLeft.addEventListener("click", () => tlContact.play());
-  //   contactRight.addEventListener("click", () => tlContact.play());
-  //   contactBack.addEventListener("click", () => tlContact.reverse());
 
   
     // let theLongDark = select("section#ipsum #the-long-dark >div");
