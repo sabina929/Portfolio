3deb219e37a11929bdba0a0750c3ae66c02e2bd9
Sabina <sabina.abbasova2909@gmail.com>
2020-05-08 19:31:03 +0400

    changed contact font size for 360px and loading text margin

--- script.js
+++ script.js
@@ -685,7 +685,8 @@ function resize() {
       gsap.set(sectionLoadertext, { scale: .36, marginTop: -690 });
     }
     if(scaleFactor<1.6 && scaleFactor >=1.5) {
-      gsap.set(sectionLoadertext, { scale: .3, marginTop: -690 });
+      // gsap.set(sectionLoadertext, { scale: .3, marginTop: -690 });
+      gsap.set(sectionLoadertext, { scale: .3, marginTop: -720 });
     }
     else if(scaleFactor<1.5 && scaleFactor >=1.4) {
       gsap.set(sectionLoadertext, { scale: .28, marginTop: -690 });
