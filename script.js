158158985f25891ba628abe15cdd002d2e64d4e7
Sabina <sabina.abbasova2909@gmail.com>
2020-05-16 21:45:35 +0400

    changed nav font size for 360px

--- script.js
+++ script.js
@@ -410,8 +410,8 @@ function loading() {
     }
     function checkMedia360ForNav() {
       if (mediaQuery360.matches) {
-        gsap.to(".to-left", {x: -180, duration: 4, ease: "linear", yoyo:true, repeat: -1})
-        gsap.to(".to-right", {x: 0, duration: 4, ease: "linear", yoyo:true, repeat: -1})      
+        gsap.to(".to-left", {x: -200, duration: 5, ease: "linear", yoyo:true, repeat: -1})
+        gsap.to(".to-right", {x: 0, duration: 5, ease: "linear", yoyo:true, repeat: -1})      
     }
     }
 
